dbe323d8952c114f166bedc021195fa31665e693
misho <michael.g.petrov@gmail.com>
2024-11-10 22:47:54 +0200

    Modified server.js and removed unused network driver

--- app/server.js
+++ app/server.js
@@ -6,9 +6,6 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-const MAX_RETRIES = 5;
-const RETRY_DELAY = 5000;
-
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -16,28 +13,17 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-function connectWithRetry(retries = 0) {
-  db.connect((err) => {
-    if (err) {
-      console.error(`Error connecting to MySQL: ${err.message}`);
-      
-      if (retries < MAX_RETRIES) {
-        console.log(`Retrying connection... Attempt ${retries + 1} of ${MAX_RETRIES}`);
-        setTimeout(() => connectWithRetry(retries + 1), RETRY_DELAY);
-      } else {
-        console.error('Max retries reached, could not connect to MySQL.');
-        process.exit(1);
-      }
-    } else {
-      console.log('Connected to MySQL');
-      
-      app.use('/', routes(db));
+db.connect((err) => {
+  if (err) {
+    console.error(`Error connecting to MySQL: ${err.message}`);
+    process.exit(1);
+  } else {
+    console.log('Connected to MySQL');
 
-      app.listen(80, () => {
-        console.log('Server running on port 80');
-      });
-    }
-  });
-}
+    app.use('/', routes(db));
 
-connectWithRetry();
+    app.listen(80, () => {
+      console.log('Server running on port 80');
+    });
+  }
+});
